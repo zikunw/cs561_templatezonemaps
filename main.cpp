11751a3db2624db0f64610dd2d3e10a4abff05a5
Aneesh Raman <aneeshr@bu.edu>
2021-01-27 11:17:36 -0500

    added test case separation

--- main.cpp
+++ main.cpp
@@ -39,13 +39,20 @@ std::vector<int> generatePointQueries(std::vector<int> data, int n)
 int main(int argc, char **argv)
 {
 
-  if(argc < 2)
+  if (argc < 3)
   {
-    std::cout<<"Usage: ./main <input_file>"<<std::endl;
+    std::cout << "Usage: ./main <input_file> <test_case>" << std::endl;
     return 0;
   }
 
   std::string input_file = argv[1];
+  std::string test_case = argv[2];
+
+  if (test_case != "test_pq" && test_case != "test_rq_1" && test_case != "test_rq_2" && test_case != "test_rq_3" && test_case != "test_rq_4")
+  {
+    std::cout << "<test_case> argument has to be one of the following: test_pq, test_rq_1, test_rq_2, test_rq_3 or test_rq_4" << std::endl;
+    return 0;
+  }
 
   // read data
   std::ifstream ifs;
@@ -60,28 +67,69 @@ int main(int argc, char **argv)
   ifs.read((char *)data.data(), filesize);
 
   //1. ----------------------------- initialize zonemap and build -----------------------------
-  //build zonemap 
-  zonemap<int> zones(data, (uint)data.size()/100);
+  //build zonemap
+  zonemap<int> zones(data, (uint)data.size() / 100);
 
+  if (test_case == "test_pq")
+  {
+    //2. ----------------------------- point queries -----------------------------
+    std::vector<int> queries = generatePointQueries(data, data.size());
 
-  //2. ----------------------------- point queries -----------------------------
-  std::vector<int> queries = generatePointQueries(data, data.size());
+    auto start = std::chrono::high_resolution_clock::now();
+    // query from zonemaps here
+    auto stop = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
+    unsigned long long point_query_time = duration.count();
+    std::cout << "Time taken to perform point queries from zonemap = " << point_query_time << " microseconds" << endl;
+  }
+  else if (test_case == "test_rq_1")
+  {
+    //3. ----------------------------- range queries -----------------------------
+    auto start = std::chrono::high_resolution_clock::now();
+    // range query from zonemaps here
+    int n = data.size();
 
-  auto start = std::chrono::high_resolution_clock::now();
-  // query from zonemaps here 
-  auto stop = std::chrono::high_resolution_clock::now();
-  auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
-  unsigned long long point_query_time = duration.count();
+    auto stop = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
+    unsigned long long range_query_time = duration.count();
+    std::cout << "Time taken to perform range query 1 from zonemap = " << range_query_time << " microseconds" << endl;
+  }
+  else if (test_case == "test_rq_2")
+  {
+    //3. ----------------------------- range queries -----------------------------
+    auto start = std::chrono::high_resolution_clock::now();
+    // range query from zonemaps here
+    int n = data.size();
 
+    auto stop = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
+    unsigned long long range_query_time = duration.count();
+    std::cout << "Time taken to perform range query 2 from zonemap = " << range_query_time << " microseconds" << endl;
+  }
+  else if (test_case == "test_rq_3")
+  {
+    //3. ----------------------------- range queries -----------------------------
+    auto start = std::chrono::high_resolution_clock::now();
+    // range query from zonemaps here
+    int n = data.size();
 
-  //3. ----------------------------- range queries -----------------------------
-  start = std::chrono::high_resolution_clock::now();
-  // range query from zonemaps here 
-  int n = data.size();
-  
-  stop = std::chrono::high_resolution_clock::now();
-  duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
-  unsigned long long range_query_time = duration.count();
+    auto stop = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
+    unsigned long long range_query_time = duration.count();
+    std::cout << "Time taken to perform range query 3 from zonemap = " << range_query_time << " microseconds" << endl;
+  }
+  else if (test_case == "test_rq_4")
+  {
+    //3. ----------------------------- range queries -----------------------------
+    auto start = std::chrono::high_resolution_clock::now();
+    // range query from zonemaps here
+    int n = data.size();
 
+    auto stop = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
+    unsigned long long range_query_time = duration.count();
+    std::cout << "Time taken to perform range query 4 from zonemap = " << range_query_time << " microseconds" << endl;
+  }
+ 
   return 0;
 }
